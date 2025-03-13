db617a7f41c851f9cd502c3b70987307843a82e3
juliaribeiro7 <juliaribeirodefigueiredo@gmail.com>
2025-03-12 22:29:46 -0300

    Add files via upload

---
+++ Atv1 Tabela 1.sql
@@ -0,0 +1,14 @@
+create database etec;
+use etec;
+
+CREATE TABLE pet
+(
+	NumRegistro int,
+    Nome varchar(80),
+    Especie varchar(25),
+    Raca varchar(30),
+    Cor varchar(40),
+    Nascimento datetime,
+    Sexo varchar(9)
+    );
+    
---
+++ Atv2  Tabela 1.sql
@@ -0,0 +1,11 @@
+create table Funcionario
+(
+CodFunc int,
+NomeFunc varchar(50),
+CodDepto int, 
+Ramal int null,
+Salario double, 
+DataAdmissao datetime, 
+DataCadastro datetime,
+Sexo char(1)
+)
